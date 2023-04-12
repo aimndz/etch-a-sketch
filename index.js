88b0559eda6f8757b6ca5690a06cd05f5177714f
Amiel Ian C. Mendoza <amielmendoza143@gmail.com>
2023-04-12 21:17:19 +0800

    add ui

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
 const containerEl = document.getElementById("container");
+const colorInputEl = document.getElementById("colorInput");
+const pixelEl = document.getElementsByClassName("pixel");
+const gridSizeEl = document.getElementById("gridSize");
+// Button
 const newCanvasBtnEl = document.getElementById("new-cnvs-btn");
+const resetBtnEl = document.getElementById("resetBtn");
+const eraseBtnEl = document.getElementById("eraseBtn");
+const paintBtnEL = document.getElementById("paintBtn");
+const randomBtnEL = document.getElementById("randomBtn");
 
 // Default
 let canvasSize = 16;
 createCanvas(canvasSize);
+
+resetBtnEl.addEventListener("click", function () {
+  for (let i = 0; i < pixelEl.length; ++i) {
+    pixelEl[i].style.backgroundColor = "#eeeeee";
+  }
+});
+
+newCanvasBtnEl.addEventListener("click", function () {
+  canvasSize = prompt("Enter grid size: ");
+
+  if (canvasSize > 0 && canvasSize <= 100) {
+    clearCanvas();
+    createCanvas(canvasSize);
+    gridSizeEl.textContent = `${canvasSize} x ${canvasSize}`;
+  } else {
+    alert("Invalid canvas size, input again.");
+  }
+});
+
+paintBtnEL.addEventListener("click", addColor);
+
+eraseBtnEl.addEventListener("click", function () {
+  for (let i = 0; i < pixelEl.length; ++i) {
+    pixelEl[i].addEventListener("mouseover", function () {
+      pixelEl[i].style.backgroundColor = "#eeeeee";
+    });
+  }
+});
+
+randomBtnEL.addEventListener("click", function () {
+  for (let i = 0; i < pixelEl.length; ++i) {
+    pixelEl[i].addEventListener("mouseover", function () {
+      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      pixelEl[i].style.backgroundColor = "#" + randomColor;
+    });
+  }
+});
 
 function createCanvas(numOfRow) {
   for (let i = 0; i < numOfRow; ++i) {
@@ -20,25 +65,16 @@ function createCanvas(numOfRow) {
   }
 
   // Hover Color
-  const pixelEl = document.getElementsByClassName("pixel");
+  addColor();
+}
 
+function addColor() {
   for (let i = 0; i < pixelEl.length; ++i) {
     pixelEl[i].addEventListener("mouseover", function () {
-      pixelEl[i].style.backgroundColor = "blue";
+      pixelEl[i].style.backgroundColor = colorInputEl.value;
     });
   }
 }
-
-newCanvasBtnEl.addEventListener("click", function () {
-  canvasSize = prompt("Enter the size of canvas: ");
-
-  if (canvasSize <= 100) {
-    clearCanvas();
-    createCanvas(canvasSize);
-  } else {
-    alert("Canvas size is too large, input again.");
-  }
-});
 
 function clearCanvas() {
   while (containerEl.firstChild) {
